9392df4bbb2626a0a8bca75175beb2c63ed0b60b
peter kuria <you@example.com>
2020-02-03 01:09:37 -0500

    added lcation model

--- gallaryapp/models.py
+++ gallaryapp/models.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+# Create your models here.
+class Location(models.Model):
+    location_name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.location_name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(location_name=value)
