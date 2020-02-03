fecd45dfe251df06fa824d99b20b4f9d6c31b996
peter kuria <you@example.com>
2020-02-03 01:28:05 -0500

    added welcome view

--- gallaryapp/views.py
+++ gallaryapp/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 from .models import Image, Category, Location
 
 # Create your views here.
+# Create your views here.
+def welcome(request):
+    images = Image.get_all_images()
+    locations = Location.objects.all()
+    return render(request, 'welcome.html', {"images": images, "locations": locations})
