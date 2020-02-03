41dddaa7c863f32d3adb9d02c9537d5b61c1b440
peter kuria <you@example.com>
2020-02-03 02:56:06 -0500

    created a suoer user

--- gallary/settings.py
+++ gallary/settings.py
@@ -120,4 +120,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# configuring the location for media
+MEDIA_URL = '/media/'
--- gallaryapp/urls.py
+++ gallaryapp/urls.py
@@ -9,3 +9,6 @@ urlpatterns=[
      url(r'^$', views.welcome, name='welcome'),
      url(r'^search/',views.search_image,name='search_image'),
      url(r'^location/(?P<location_name>\w+)/', views.get_image_by_location, name='location'),]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
