6584be81e00228ce9102ac606e87c4771bf74ca5
peter kuria <you@example.com>
2020-02-03 01:41:47 -0500

    added urls

---
+++ gallaryapp/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+
+
+urlpatterns=[
+     url(r'^$', views.welcome, name='welcome'),
+     url(r'^search/',views.search_image,name='search_image'),
+     url(r'^location/(?P<location_name>\w+)/', views.get_image_by_location, name='location'),]
